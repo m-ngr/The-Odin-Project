adbe2a8030aaea4ce185e2340d7b85349872521f
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-01-13 14:15:33 +0200

    sumAll exercise completed

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)) {return "ERROR";}
+    if ( 0 > a|| 0 > b) {return "ERROR";}
 
+    let from = Math.min(a, b);
+    let to = Math.max(a, b);
+    let sum = 0;
+    for(let n = from; n <= to; ++n){
+        sum += n;
+    }
+    return sum;
 };
 
 // Do not edit below this line
