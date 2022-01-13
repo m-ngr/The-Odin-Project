029099ba077c2c2eff6297886219816741193d88
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-01-13 12:59:41 +0200

    repeatString exercise completed

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0){return 'ERROR';}
+    let res = '';
+    for(let n = 1 ; n <= num; ++n){
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
