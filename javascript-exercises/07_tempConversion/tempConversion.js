6ba24f29794043f87356df5929dd764656082e7c
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-01-13 14:42:02 +0200

    tempConversion exercise completed

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = ((tempF - 32) * 5 / 9);
+  return Number(tempC.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = (tempC * 9 / 5 + 32);
+  return Number(tempF.toFixed(1));
 };
 
 // Do not edit below this line
