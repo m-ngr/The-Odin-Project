64827433480e2b93dfb1b5b3138347254deffae8
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-01-13 14:28:07 +0200

    leapYears exercise completed

--- javascript-exercises/06_leapYears/leapYears.js
+++ javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year%4 != 0){return false;}
+    if(year%400 == 0){return true;}
+    if(year%100 == 0){return false;}
+    return true;
 };
 
 // Do not edit below this line
