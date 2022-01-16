f73f997291c99e1aa301ddff13821441d6bfc36f
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-01-16 16:07:22 +0200

    UI version of the project is completed

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,91 +1,188 @@
-const WIN = true;  // player win, computer lost
+const WIN = true; // player win, computer lost
 const LOST = false; // player lost, computer win
-const TIE = null; 
+const TIE = null;
 const INVALID = undefined;
+//argh, I hate global variable
+let round = 0;
+let computerScore = 0;
+let playerScore = 0;
+let maxRounds = 5;
+//====================================================================================================
+document.querySelector("#play-btn").addEventListener("click", startGame);
 
-function game(rounds = 5){
-    console.clear()
-    let computerScore = 0;
-    let playerScore = 0;
+function startGame(e) {
+  e.currentTarget.removeEventListener("click", startGame);
+  e.currentTarget.disabled = true;
 
-    for(let r = 1; r <= rounds; ++r){
-        let playerSelection = prompt("Enter only one of the following:\nrock, paper, or scissors","");
-        playerSelection = capitalize(playerSelection);
-        let computerSelection = computerPlay();
-        let roundResult = playRound(playerSelection ,computerSelection);
+  const selections = document.querySelectorAll("#selections > button");
+  selections[0].addEventListener("click", () => play("Rock"));
+  selections[1].addEventListener("click", () => play("Paper"));
+  selections[2].addEventListener("click", () => play("Scissors"));
 
-        console.log (`Round #${r}: ${showRoundResult(roundResult, playerSelection, computerSelection)}`);
+  selections.forEach((btn) => (btn.disabled = false));
 
-        if (roundResult === INVALID) {--r; continue;} //repeat this round
-        if (roundResult === TIE) {continue;} //goto next round
-        //update scores
-        if (roundResult === WIN) {
-            ++playerScore;
-        }else{
-            ++computerScore;
-        }
-    }
-    //print game result
-    if (playerScore > computerScore){
-        console.log("Game Over: You Win");
-    }else if (playerScore < computerScore){
-        console.log("Game Over: You Lost");
-    }else{
-        console.log("Game Over: Tie");
-    }
+  const numInput = document.getElementById("rounds");
+  numInput.disabled = true;
 
+  if (numInput.value <= 0 || numInput.value > 100) {
+    numInput.value = 5;
+  }
+
+  setGameInfo(numInput.value);
+  showGameInfo();
 }
 
-function capitalize(str){ return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();}
+function setGameInfo(maxRoundsvalue) {
+  maxRounds = maxRoundsvalue;
+  round = 0;
+  playerScore = 0;
+  computerScore = 0;
 
-function showRoundResult(roundResult, playerSelection, computerSelection){
+  const gameResultPara = document.querySelector("#game-result");
+  gameResultPara.textContent = "";
+  gameResultPara.className = "";
 
-    if(roundResult === INVALID) {return "Please, provide a proper input"; }
-    if(roundResult === TIE) {return `Tie! both chose ${playerSelection}`;}
-
-    if(roundResult === WIN) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }else{
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
+  const roundResultPara = document.querySelector("#result");
+  roundResultPara.textContent = "";
+  roundResultPara.className = "";
 }
 
-function playRound(playerSelection, computerSelection){
-
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
-    switch(true){
-        case playerSelection == "rock"     && computerSelection == "paper":
-        case playerSelection == "paper"    && computerSelection == "scissors":
-        case playerSelection == "scissors" && computerSelection == "rock":
-            return LOST;
-
-        case playerSelection == "rock"     && computerSelection == "scissors":
-        case playerSelection == "paper"    && computerSelection == "rock":
-        case playerSelection == "scissors" && computerSelection == "paper":
-            return WIN
-
-        case playerSelection == "rock"     && computerSelection == "rock":
-        case playerSelection == "paper"    && computerSelection == "paper":
-        case playerSelection == "scissors" && computerSelection == "scissors":
-            return TIE
-
-        default:
-            return INVALID;
-    }
+function play(playerSelection) {
+  ++round;
+  let computerSelection = computerPlay();
+  let roundResult = playRound(playerSelection, computerSelection);
+  showRoundResult(round, roundResult, playerSelection, computerSelection);
+  showGameInfo();
+  //is game over
+  if (round >= maxRounds) {
+    endGame();
+  }
 }
 
-function computerPlay(){
-    let ran = Math.round(Math.random()*2);
-    switch(ran){
-        case 0:
-            return "Rock";
-        case 1:
-            return "Paper";
-        case 2:
-            return "Scissors";
-        default:
-            return computerPlay();
-    }
+function endGame() {
+  showGameResult();
+
+  const playBtn = document.getElementById("play-btn");
+  playBtn.addEventListener("click", startGame);
+  playBtn.disabled = false;
+
+  document.getElementById("rounds").disabled = false;
+
+  const selections = document.querySelectorAll("#selections > button");
+  selections.forEach((btn) => (btn.disabled = true));
+  //replacing old nodes with new clones to remove the event listener
+  selections.forEach((btn) => {
+    let new_btn = btn.cloneNode(true);
+    btn.parentNode.replaceChild(new_btn, btn);
+  });
+}
+
+function showGameInfo() {
+  document.getElementById(
+    "player-score"
+  ).textContent = `Your Score: ${playerScore}`;
+
+  document.getElementById(
+    "computer-score"
+  ).textContent = `Computer Score: ${computerScore}`;
+
+  document.getElementById("round-info").textContent = `Round: ${Math.min(
+    round + 1,
+    maxRounds
+  )}/${maxRounds}`;
+}
+
+function showGameResult() {
+  const resultPara = document.querySelector("#game-result");
+  if (playerScore > computerScore) {
+    resultPara.textContent = "Game Over: You Win";
+    resultPara.className = "win";
+  } else if (playerScore < computerScore) {
+    resultPara.textContent = "Game Over: You Lost";
+    resultPara.className = "lost";
+  } else {
+    resultPara.textContent = "Game Over: Tie";
+    resultPara.className = "tie";
+  }
+}
+
+function showRoundResult(
+  roundNumber,
+  roundResult,
+  playerSelection,
+  computerSelection
+) {
+  const resultPara = document.querySelector("#result");
+  resultPara.textContent = `Round #${roundNumber}: ${getRoundResult(
+    roundResult,
+    playerSelection,
+    computerSelection
+  )}`;
+
+  if (roundResult === WIN) {
+    resultPara.className = "win";
+  } else if (roundResult === LOST) {
+    resultPara.className = "lost";
+  } else if (roundResult === TIE) {
+    resultPara.className = "tie";
+  } else {
+    resultPara.className = "";
+  }
+}
+//=======================================================================================================================
+
+function getRoundResult(roundResult, playerSelection, computerSelection) {
+  if (roundResult === INVALID) {
+    return "Please, provide a proper input";
+  }
+  if (roundResult === TIE) {
+    return `Tie! both chose ${playerSelection}`;
+  }
+
+  if (roundResult === WIN) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  switch (true) {
+    case playerSelection == "rock" && computerSelection == "paper":
+    case playerSelection == "paper" && computerSelection == "scissors":
+    case playerSelection == "scissors" && computerSelection == "rock":
+      computerScore += 1;
+      return LOST;
+
+    case playerSelection == "rock" && computerSelection == "scissors":
+    case playerSelection == "paper" && computerSelection == "rock":
+    case playerSelection == "scissors" && computerSelection == "paper":
+      playerScore += 1;
+      return WIN;
+
+    case playerSelection == "rock" && computerSelection == "rock":
+    case playerSelection == "paper" && computerSelection == "paper":
+    case playerSelection == "scissors" && computerSelection == "scissors":
+      return TIE;
+
+    default:
+      return INVALID;
+  }
+}
+
+function computerPlay() {
+  let ran = Math.round(Math.random() * 2);
+  switch (ran) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+    default:
+      return computerPlay();
+  }
 }
