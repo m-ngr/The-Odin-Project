7b7e1facc4e792a22a2c53a7a3fb218b166a8dcd
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-01-17 20:08:56 +0200

    findTheOldest exercise completed

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+  const age = (p) =>
+    (p.yearOfDeath ?? new Date().getFullYear()) - p.yearOfBirth;
 
+  return people.reduce((oldest, person) => {
+    if (oldest === undefined) return person;
+    return age(oldest) < age(person) ? person : oldest;
+  });
 };
 
 // Do not edit below this line
