bef2bb0c522fff66fec20de55db73e4b13e70128
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-01-17 19:21:30 +0200

    palindromes exercise completed

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let chars = str
+    .toLowerCase()
+    .split("")
+    .filter((ch) => ch >= "a" && ch <= "z");
+  while (chars.length > 1) {
+    if (chars.shift() !== chars.pop()) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
