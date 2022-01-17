d57d8752a4badef1a9fa3e4eec51a38a4ba3f100
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-01-17 19:41:38 +0200

    fibonacci exercise completed

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = (number) => {
+  if (number < 1) return "OOPS";
+  if (number < 3) return 1;
+  // using result object to memorize results, so we don't compute them again.
+  if (fibonacci.result === undefined) fibonacci.result = {};
+  if (!(number in fibonacci.result)) {
+    fibonacci.result[number] = fibonacci(+number - 1) + fibonacci(+number - 2);
+  }
+  return fibonacci.result[number];
 };
 
 // Do not edit below this line
