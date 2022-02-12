e5b47ea77fe142f6030b7cffc288a8d5221a700b
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-02-12 18:37:15 +0200

    Project completed

--- sign-up-form/app.js
+++ sign-up-form/app.js
@@ -1,41 +1,7 @@
-const REG = {
-  name: {
-    exp: /^[a-zA-Z- ]{3,20}$/,
-    msg: "This field can only contain 3:20 alpha, space, hyphen",
-  },
-  email: {
-    exp: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-    msg: "This field expects an email in form: example@domain.com",
-  },
-  phone: {
-    exp: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
-    msg: "This field expects a phone number in form: 0123456789",
-  },
-  password: {
-    exp: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
-    msg: "Minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character",
-  },
-};
-
 document.getElementById("mode").addEventListener("change", toggleTheme);
-document
-  .getElementById("first-name")
-  .addEventListener("input", (e) => validate(e, REG.name));
-document
-  .getElementById("last-name")
-  .addEventListener("input", (e) => validate(e, REG.name));
-document
-  .getElementById("email")
-  .addEventListener("input", (e) => validate(e, REG.email));
-document
-  .getElementById("phone")
-  .addEventListener("input", (e) => validate(e, REG.phone));
-document
-  .getElementById("pwd")
-  .addEventListener("input", (e) => validate(e, REG.password));
-document
-  .getElementById("cnf-pwd")
-  .addEventListener("input", (e) => validate(e, REG.password));
+
+document.getElementById("pwd").addEventListener("input", matchPwd);
+document.getElementById("cnf-pwd").addEventListener("input", matchPwd);
 
 function toggleTheme(e) {
   if (this.checked) {
@@ -45,19 +11,17 @@ function toggleTheme(e) {
   }
 }
 
-function validate(e, reg) {
-  if (reg.exp.test(e.target.value.trim())) {
-    e.target.setCustomValidity("");
+function matchPwd(e) {
+  const pwd1 = document.getElementById("pwd");
+  const pwd2 = document.getElementById("cnf-pwd");
+  const out = document.querySelector('output[for="cnf-pwd"]');
+  if (pwd1.validity.valid && pwd2.value !== "" && pwd1.value !== pwd2.value) {
+    out.style.height = "auto";
+    out.style.visibility = "visible";
+    pwd2.style.borderColor = "red";
   } else {
-    e.target.setCustomValidity(reg.msg);
-    e.target.reportValidity();
-  }
-  if (
-    reg === REG.password &&
-    document.getElementById("pwd").value !==
-      document.getElementById("cnf-pwd").value
-  ) {
-    e.target.setCustomValidity("passwords don't match");
-    e.target.reportValidity();
+    out.style.height = "0";
+    out.style.visibility = "hidden";
+    pwd2.style.borderColor = "";
   }
 }
