bb99000d8998b8ac4d480091bda997019b54dba5
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-04-06 10:57:06 +0200

    header buttons implemented

--- todo/src/app/app.js
+++ todo/src/app/app.js
@@ -1,1 +1,19 @@
-export default function run() {}
+export default function run() {
+  document.getElementById("theme-btn").addEventListener("click", toggleTheme);
+  document
+    .getElementById("sidebar-btn")
+    .addEventListener("click", toggleSidebar);
+}
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    this.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  } else {
+    this.innerHTML = '<i class="fa-solid fa-sun"></i>';
+  }
+}
+
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("hide");
+}
