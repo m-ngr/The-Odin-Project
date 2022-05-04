e024f682820f9e96238030434b614abcc56a01c5
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-05-04 16:27:46 +0200

    initial commit

---
+++ weather/webpack.common.js
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "assets/[name].[hash][ext][query]",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
