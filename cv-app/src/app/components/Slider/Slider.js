24a25265d8d1d19a8a42b81e6da119c4d53d96bf
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-05-28 13:53:37 +0200

    fixing typo

--- cv-app/src/app/components/Slider/Slider.js
+++ cv-app/src/app/components/Slider/Slider.js
@@ -10,7 +10,6 @@ export default class Slider extends Component {
   render() {
     return (
       <section className="Slider">
-        <></>
         {this.getCurrentSlide()}
         {this.backButton()}
         {this.nextButton()}
