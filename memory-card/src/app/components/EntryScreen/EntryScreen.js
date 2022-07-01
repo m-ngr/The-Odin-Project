6f15298f87f573f2c9f0cae33aa59ef854e090a6
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-07-01 14:35:22 +0200

    cleaning

--- memory-card/src/app/components/EntryScreen/EntryScreen.js
+++ memory-card/src/app/components/EntryScreen/EntryScreen.js
@@ -1,12 +1,10 @@
+import entryImg from "../../../images/entry.jpg";
+
 export function EntryScreen({ level, updateLevel, startHandler }) {
   return (
     <section className="flex-col">
       <h2>Welcome to Memory Card</h2>
-      <img
-        src="https://wallpapercave.com/wp/wp9908967.jpg"
-        alt="wallpaper"
-        height="300px"
-      />
+      <img src={entryImg} alt="wallpaper" height="300px" />
       <div className="input-wrapper">
         <label htmlFor="entry-level"> Choose Level</label>
         <input
